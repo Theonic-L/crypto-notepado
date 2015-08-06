7e0b97f5d5bdefd55f9360b25beb28e70d8669d7
Theonic-L <theonic184@vivaldi.net>
2015-08-06 16:52:47 +0300

    Test

--- PasswordManager/Data.cs
+++ PasswordManager/Data.cs
@@ -8,7 +8,7 @@ namespace PasswordManager
 {
     public static class Data
     {
-        public static string salt = "ooo";//"But I, being poor, have only my dreams*** I have spread my dreams under your feet**** Tread softly because you tread on my dreams.";
+        public static string salt = "ooo";//"HBut I, being poor, have only my dreams*** I have spread my dreams under your feet**** Tread softly because you tread on my dreams.";
         public static string vector = "OnRnp73m*Dze01QWqwD";//"VeCtOr(iAnnc iedpetai iosn like a virus. Resilient. Highly contagious.)";
         public static string saltPass = "lingvafon-terrible-10___:lk$$$@()void";//
     }
